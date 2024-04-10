cdaeb42ee161b59ab3bb6b8ac5ee64770d8025c6
Gil <jcg3417@gmail.com>
2024-04-11 00:04:19 +0100

    querySelector test

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const inputForm = document.querySelector(".input-form")
 console.log(inputForm)
+
+const input = inputForm.input
+console.log(input)
+const logInBtn = inputForm.logInBtn
+console.log(logInBtn)

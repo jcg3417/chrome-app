7203d662bfeadda8ee924d28c7c37a2563efd120
Gil <jcg3417@gmail.com>
2024-04-18 21:05:25 +0100

    HIDDEN_CLASSNAME variable added

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const form = document.querySelector(".input-form")
-console.log(form)
 const input = form.querySelector(".input")
-console.log(input)
 const logInBtn = form.querySelector(".logInBtn")
-console.log(logInBtn)
+const HIDDEN_CLASSNAME = "hidden"
 
 function handleLogInBtn() {
     const lengthOfName = input.value.length
-    const userName = input.value
 
-    if (input.value === "") {
+    const userName = input.value
+    console.log(userName)
+
+    if (userName === "") {
         alert("Please type in your name!")
     } else if (lengthOfName <= 1) {
         alert("You name is too short!")
     } else if (lengthOfName > 15) {
         alert("Your name is too long!")
     } else {
-        form.classList.add("hidden")
+        form.classList.add(HIDDEN_CLASSNAME)
     }
-    console.log("Clicked")
-    console.dir(input)
-    console.log(lengthOfName)
 }
 
 logInBtn.addEventListener("click", handleLogInBtn)

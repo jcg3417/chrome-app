eb47052ca736be2857af5b72c447f47d46d132a1
Gil <jcg3417@gmail.com>
2024-04-18 21:15:42 +0100

    Hidden H1 added in HTML

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const form = document.querySelector(".input-form")
 const input = form.querySelector(".input")
 const logInBtn = form.querySelector(".logInBtn")
 const HIDDEN_CLASSNAME = "hidden"
+const body = document.body.innerText
+
+console.log(body)
 
 function handleLogInBtn() {
     const lengthOfName = input.value.length

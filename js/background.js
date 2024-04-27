99116f137c75e9f64edd4a0b95f81a0f7a337ad2
Gil <jcg3417@gmail.com>
2024-04-27 23:27:47 +0100

    Add background img tag to index.html

--- js/background.js
+++ js/background.js
@@ -1,1 +1,16 @@
+// Image array
 const images = ["0.jpeg", "1.jpeg", "2.jpeg"]
+
+// Choose random image
+const chosenImg = Math.floor(Math.random() * images.length)
+const backgroundImage = images[chosenImg]
+console.log(backgroundImage)
+
+// Add a new img tag
+const newImageTag = document.createElement("img")
+console.log(newImageTag)
+newImageTag.src = `${backgroundImage}`
+// console.log(newImageTag.src)
+
+const divAfter = document.getElementById("quoteSection")
+document.body.after(newImageTag, divAfter)

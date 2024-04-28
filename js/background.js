1b681791df36de2e2df048596c0522642b7617fa
Gil <jcg3417@gmail.com>
2024-04-28 14:00:10 +0100

    Delete pointless console.logs and create todolist.js

--- js/background.js
+++ js/background.js
@@ -3,12 +3,10 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg"]
 
 // Choose random image
 const chosenImg = Math.floor(Math.random() * images.length)
-const backgroundImage = images[chosenImg]
-console.log(backgroundImage)
+const backgroundImage = images[chosenImg])
 
 // Add a new img tag
 const newImageElement = document.createElement("img")
-console.log(newImageElement)
 newImageElement.src = `images/${backgroundImage}`
 
 // Append background element
